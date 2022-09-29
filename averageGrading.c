40c64c79b85613a179d7d8494795e306a747468c
imgrnt <114675092+imgrnt@users.noreply.github.com>
2022-09-29 10:16:28 -0400

    Add files via upload

---
+++ averageGrading.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main() {
+    int numGrades = 0;
+    double avg = 0.0;
+    int grade = 0;
+    int total = 0;
+
+    do {
+        printf("Enter a grade #%d: ", numGrades+1);
+        scanf_s("%d", &grade);
+
+            if (grade != -99) {
+                total += grade;
+                numGrades++;
+            }
+            
+    } while (grade != -99);
+
+    avg = total / (double) numGrades;
+    printf("%f", avg);
+    return 0;
+}
